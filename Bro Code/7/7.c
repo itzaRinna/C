eb7eb3151e1d00a975ecf35e0edd7312546d0974
itzaRinna <dutchismahdick@gmail.com>
2024-05-16 12:57:09 +0100

    new files

---
+++ Bro Code/7/7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    // arithmetic operators
+ 
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ increment
+    // -- decrement
+ 
+    int x = 5;
+    int y = 2;
+    
+    // int z = x + y;
+    // int z = x - y;
+    // int z = x * y;
+    // float z = x / (float) y;
+    int z = x % y;
+    // x++;
+    // y--;
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+    printf("%d\n", z);
+    
+    return 0;
+}
